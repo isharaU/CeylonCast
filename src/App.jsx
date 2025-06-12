29204d91b669aade5c4f059da85f6535b10bb681
YisharaU <wellahewawiud.20@uom.lk>
2025-06-12 17:39:24 +0530

    update title and add Google icons link in index.html; include search icon in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   return (
     <div className="container">
       <form action="" className="search-form">
+        <span className="material-symbols-outlined">
+          search
+        </span>
         <input type='search' className='search-input' placeholder='Enter a City Name'/> 
         <button type='submit' className='search-button'>Search</button>
       </form>
