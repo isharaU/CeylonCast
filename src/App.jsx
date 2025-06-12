a3c0fb78c2980fcdfd7c9fda9e2e965eb55c9131
YisharaU <wellahewawiud.20@uom.lk>
2025-06-12 17:33:48 +0530

    fix formatting of input element in search form

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="container">
       <form action="" className="search-form">
-        <input type='search' className='search-input' placeholder='Enter a City Name' />
+        <input type='search' className='search-input' placeholder='Enter a City Name'/> 
         <button type='submit' className='search-button'>Search</button>
       </form>
     </div>
