4c3f2d68ce13fbdeb14e0c8a4cfc2eca25feb289
YisharaU <wellahewawiud.20@uom.lk>
2025-06-12 17:32:23 +0530

    add initial App component with search form

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div className="container">
+      <form action="" className="search-form">
+        <input type='search' className='search-input' placeholder='Enter a City Name' />
+        <button type='submit' className='search-button'>Search</button>
+      </form>
+    </div>
+  )
+}
+export default App;
