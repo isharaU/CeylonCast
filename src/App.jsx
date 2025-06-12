ae07aa39274a688947b386303848bae67538b641
YisharaU <wellahewawiud.20@uom.lk>
2025-06-12 18:20:02 +0530

    refactor layout of search section in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,24 @@ const App = () => {
     <div className="container">
 
       {/* Search Section */}
-      <form action="" className="search-form">
-        <span className="material-symbols-outlined">
-          search
-        </span>
-        <input type='search' className='search-input' placeholder='Enter a City Name'/> 
-        <button type='submit' className='search-button'>Search</button>
-      </form>
+      <div className='search-container'>
+        <form action="" className="search-form">
+          <span className="material-symbols-outlined">
+            search
+          </span>
+          <input type='search' className='search-input' placeholder='Enter a City Name'/> 
+          <button type='submit' className='search-button'>Search</button>
+        </form>
+
+        <button className='location-button'>
+          <span className="material-symbols-outlined">
+            my_location
+          </span>
+        </button>
+      </div>
 
 
-      <button className='location-button'>
-        <span className="material-symbols-outlined">
-          my_location
-        </span>
-        
-      </button>
+ 
 
     </div>
 
