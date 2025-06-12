170937a0e85ea243281cbd86f2000fcca0d4c470
YisharaU <wellahewawiud.20@uom.lk>
2025-06-12 17:47:51 +0530

    add location button and update icon links in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 const App = () => {
   return (
     <div className="container">
+
+      {/* Search Section */}
       <form action="" className="search-form">
         <span className="material-symbols-outlined">
           search
@@ -13,21 +15,15 @@ const App = () => {
       </form>
 
 
-      <button className='search-button'>Search by Coordinates</button>
-      <div className='weather-info'>
-        <h2 className='city-name'>City Name</h2>
-        <p className='temperature'>Temperature: 25°C</p>
-        <p className='condition'>Condition: Sunny</p>
-        <p className='humidity'>Humidity: 60%</p>
-        <p className='wind-speed'>Wind Speed: 10 km/h</p>
-        <p className='pressure'>Pressure: 1012 hPa</p>
-        <p className='visibility'>Visibility: 10 km</p>
-        <p className='sunrise'>Sunrise: 6:00 AM</p>
-        <p className='sunset'>Sunset: 6:00 PM</p>
-        <p className='timezone'>Timezone: GMT+0</p>
-        <p className='last-updated'>Last Updated: 12:00 PM</p>      
-      </div>
+      <button className='location-button'>
+        <span className="material-symbols-outlined">
+          my_location
+        </span>
+        
+      </button>
+
     </div>
+
   );
 }
 export default App;
