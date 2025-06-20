30da19ad63c183cd7976aa8897f64d53ba209979
YisharaU <wellahewawiud.20@uom.lk>
2025-06-20 21:33:47 +0530

    refactor HourlyForecast component and CSS for improved layout and styling

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -3,9 +3,9 @@ import cloudyIcon from '../assets/cloudy.png';
 
 const HourlyForecast = ({ hour }) => {
   return (
-    <div className="hourly-forecast" align="center">
+    <div className="hourly-forecast">
       <ul className="hourly-list">
-        <li className="hourly-item">
+        <li className="hourly-item" key={hour.time}>
           <span className="hour">{hour.time.split(" ")[1].substring(0, 5)}</span>
           <img src={hour.icon} alt={hour.description} className="hourly-icon" width={"20%"} height={"auto"} />
           <span className="hourly-temp">{hour.temperature}°C</span>
