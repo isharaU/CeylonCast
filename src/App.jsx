68c2c45a1218a2e549d6764942fe131d6e45a8c2
YisharaU <wellahewawiud.20@uom.lk>
2025-06-20 20:25:17 +0530

    refactor useWeather hook to manage no results state and update App component for conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,33 @@ import HourlyForecast from './components/HourlyForecast';
 import { useWeather } from './hooks/useWeather';
 
 const App = () => {
-  const { currentWeather, hourlyForecast, getWeatherDetails } = useWeather();
+  const { currentWeather, hourlyForecast, hasNoResults, getWeatherDetails } = useWeather();
 
   return (
     <div className="container">
       <Search getWeatherDetails={getWeatherDetails} />
       
-      <div className="weather-container">
-        <CurrentWeather weather={currentWeather} />
-      </div>
+      {hasNoResults ? (
+        <div className="no-results">
+          <p>No results found. Please try a different search.</p>
+        </div>
+      ):(
+      <>
+        <div className="weather-container">
+          <CurrentWeather weather={currentWeather} />
+        </div>
 
-      <div className="hourly-forecast-container overflow-x-auto flex space-x-4 p-4">
-        {hourlyForecast.length > 0 ? (
-          hourlyForecast.map((hour, index) => (
-            <HourlyForecast key={index} hour={hour} />
-          ))
-        ) : (
-          <p>No hourly forecast available.</p>
-        )}
-      </div>
+        <div className="hourly-forecast-container overflow-x-auto flex space-x-4 p-4">
+          {hourlyForecast.length > 0 ? (
+            hourlyForecast.map((hour, index) => (
+              <HourlyForecast key={index} hour={hour} />
+            ))
+          ) : (
+            <p>No hourly forecast available.</p>
+          )}
+        </div>
+      </>
+      )}
     </div>
   );
 };
--- src/hooks/useWeather.jsx
+++ src/hooks/useWeather.jsx
@@ -4,8 +4,10 @@ import { parseWeatherData } from '../utils/weatherService';
 export const useWeather = () => {
   const [currentWeather, setCurrentWeather] = useState({});
   const [hourlyForecast, setHourlyForecast] = useState([]);
+  const [hasNoResults, setHasNoResults] = useState(false);
 
   const getWeatherDetails = async (API_URL) => {
+    setHasNoResults(false);
     try {
       const response = await fetch(API_URL);
       if (!response.ok) throw new Error("Network response was not ok");
@@ -16,9 +18,10 @@ export const useWeather = () => {
       setCurrentWeather(currentWeather);
       setHourlyForecast(hourlyForecast);
     } catch (error) {
+      setHasNoResults(true);
       console.error("Error fetching weather data:", error);
     }
   };
 
-  return { currentWeather, hourlyForecast, getWeatherDetails };
+  return { currentWeather, hourlyForecast, getWeatherDetails, hasNoResults };
 };
