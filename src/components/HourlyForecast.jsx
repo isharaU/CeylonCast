eb871b55722e960b06eb0bd9d418253ddccda102
YisharaU <wellahewawiud.20@uom.lk>
2025-06-22 21:11:16 +0530

    fix: restructure HourlyForecast component layout for improved styling and responsiveness

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -3,13 +3,15 @@ import './HourlyForecast.css';
 const HourlyForecast = ({ hour }) => {
   return (
     <div className="hourly-forecast">
-      <ul className="hourly-list">
-        <li className="hourly-item" key={hour.time}>
-          <span className="hour">{hour.time.split(" ")[1].substring(0, 5)}</span>
-          <img src={hour.icon} alt={hour.description} className="hourly-icon" width={"20%"} height={"auto"} />
-          <span className="hourly-temp">{hour.temperature}°C</span>
-        </li>
-      </ul>
+      <div className="hourly-forecast-container">
+        <ul className="hourly-list">
+          <li className="hourly-item" key={hour.time}>
+            <span className="hour">{hour.time.split(" ")[1].substring(0, 5)}</span>
+            <img src={hour.icon} alt={hour.description} className="hourly-icon" width="20%" height="auto" />
+            <span className="hourly-temp">{hour.temperature}°C</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
